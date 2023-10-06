ae2c164e2cbb7f487db8f8983ace4c155edc5450
Andrea Valla <valla.andrea@gmail.com>
2023-10-06 09:48:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+import TelegramBot from 'node-telegram-bot-api';
+
+import dotenv from 'dotenv';
+import format from 'date-fns/format/index.js';
+import locale from 'date-fns/locale/it/index.js';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow/index.js';
+import reserve from './services/reserve.js';
+
+dotenv.config();
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const RICETTE = new Set();
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+console.log('Bot started... 🚀');
+
+await bot.setMyCommands([
+  { command: 'prenota', description: 'prenota <codice fiscale> <ricetta>' },
+  { command: 'help', description: 'Mostra un help' },
+]);
+
+function printMsgInfo(msg) {
+  const chatId = msg.chat.id;
+
+  const { username, first_name, last_name } = msg.from;
+  console.log(
+    `${format(msg.date, 'd MMMM yyyy H:mm', { locale })}: "${
+      msg.text
+    }" in ${chatId} from @${username} (${first_name} ${last_name})`
+  );
+}
+
+bot.onText(/\/help/, async (msg) => {
+  const chatId = msg.chat.id;
+  printMsgInfo(msg);
+  return await bot.sendMessage(
+    chatId,
+    `I comandi disponibili sono i seguenti:
+- prenota: Richiedi prenotazione <codice fiscale> <ricetta>
+- help: Questo help`
+  );
+});
+
+bot.onText(
+  /\/prenota ((?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]) (010A2[0-9]+)/i,
+  async (msg, match) => {
+    const chatId = msg.chat.id;
+    printMsgInfo(msg);
+    const [_, cf, ricetta] = match;
+    if (RICETTE.has(ricetta)) {
+      await bot.sendMessage(chatId, `Sto già cercando di prenotare questa ricetta!`);
+      return;
+    }
+    RICETTE.add(ricetta);
+    let result = {};
+    let counter = 0;
+
+    function randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    while (!result.confirmed) {
+      const randomMinutes = randomIntFromInterval(5, 10);
+      const randomSeconds = randomIntFromInterval(55, 65);
+      try {
+        result = await reserve({ cf, ricetta, counter });
+      } catch (error) {
+        await bot.sendMessage(chatId, `Scusa, c\'è stato un errore :( ${error}`);
+        console.error(error);
+      }
+      await bot.sendMessage(
+        chatId,
+        `Tra circa ${Math.round(
+          (randomMinutes * randomSeconds) / 60
+        )} minuti proverò a cercare un appuntamento per la ricetta ${ricetta} tentativo ${counter}`
+      );
+      await new Promise((r) => setTimeout(r, randomMinutes * randomSeconds * 1_000));
+      counter++;
+    }
+    if (result.confirmed && result.image) {
+      await bot.sendPhoto(chatId, result.image);
+    }
+    if (!result.confirmed) {
+      await bot.sendMessage(chatId, `${result.info}\n${result.appuntamenti.map((a) => a.text).join('\n')}`);
+      return;
+    }
+    const daysToNow = formatDistanceToNow(result.confirmed.date, { locale });
+    const friendlyDate = format(result.confirmed.date, 'EEEE d MMMM yyyy H:mm', { locale });
+    await bot.sendMessage(
+      chatId,
+      `Ho prenotato tra ${daysToNow} 🍾\n${result.info}\n${result.confirmed.address}\n${friendlyDate}`
+    );
+    RICETTE.delete(ricetta);
+  }
+);
---
+++ services/reserve.js
@@ -0,0 +1,99 @@
+import puppeteer from 'puppeteer';
+import locale from 'date-fns/locale/it/index.js';
+import parse from 'date-fns/parse/index.js';
+import format from 'date-fns/format/index.js';
+import differenceInCalendarDays from 'date-fns/differenceInCalendarDays/index.js';
+
+const CUP_URL = 'https://cup.isan.csi.it/web/guest/ricetta-dematerializzata';
+
+/**
+ * Reserve
+ * @param cf
+ * @param numeroRicetta
+ * @param counter
+ * @returns {Promise<{appuntamenti: [{date: Date, address: string, isGood: boolean}], confirmed: {date: Date, address: string, isGood: boolean}, image: Buffer, info: string}>}
+ */
+async function reserve({ cf, ricetta: numeroRicetta, counter = 0 }) {
+  console.log(`Cerco di prenotare ${cf} ${numeroRicetta} tentativo ${counter}`);
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    args: [`--window-size=1920,1080`],
+    defaultViewport: { width: 1920, height: 1080 },
+  });
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(2 * 60_000);
+  page.setDefaultTimeout(2 * 60_000);
+  const result = {
+    info: undefined,
+    confirmed: undefined,
+    appuntamenti: [],
+    image: undefined,
+  };
+  await page.goto(CUP_URL, { waitUntil: 'networkidle2' });
+  await page.$eval('input.codice-fiscale-bt', (el, value) => (el.value = value), cf);
+  await page.$eval('input.nreInput-bt', (el, value) => (el.value = value), numeroRicetta);
+  result.image = await page.screenshot({ fullPage: true });
+  await page.click(
+    'button[name="_ricettaelettronica_WAR_cupprenotazione_\\:ePrescriptionSearchForm:nreButton_button"]'
+  );
+  await page.waitForSelector(
+    'button[name="_ricettaelettronica_WAR_cupprenotazione_\\:navigation-epPrestazioni-main:epPrestazioni-nextButton-main_button"]'
+  );
+  // PAGE 2
+  const infos = await page.$$('.prestazioneRow .infoValue');
+  const info = await infos[2]?.evaluate((el) => el.textContent);
+  result.info = `${info}\n`;
+  result.image = await page.screenshot({ fullPage: true });
+  await page.click(
+    'button[name="_ricettaelettronica_WAR_cupprenotazione_:navigation-epPrestazioni-main:epPrestazioni-nextButton-main_button"]'
+  );
+  await page.waitForSelector('[name="_ricettaelettronica_WAR_cupprenotazione_:appuntamentiForm"],.no-available');
+  result.image = await page.screenshot({ fullPage: true });
+  await page.click('span[aria-describedby="Altre disponibilità"] button');
+  await page.waitForSelector('#availableAppointmentsBlock');
+  await new Promise((r) => setTimeout(r, 5_000));
+  result.image = await page.screenshot({ fullPage: true });
+  const appuntamenti = await page.$$('#availableAppointmentsBlock .appuntamento');
+  for (const appuntamento of appuntamenti) {
+    const data = (await appuntamento.$eval('.captionAppointment-dateApp', (el) => el.textContent))
+      .replace(/\n\t*/, '')
+      .replace('alle ore', ' ');
+    const address = (await appuntamento.$eval('.captionAppointment-address', (el) => el.getAttribute('data-address')))
+      .replace('\n', '')
+      .replace('null', 'N/A');
+    const date = parse(data, 'EEEE d MMMM yyyy hh:mm', new Date(), { locale });
+    const difference = differenceInCalendarDays(date, new Date());
+    if (difference === 0) {
+      console.log(`${numeroRicetta} C'è poco tempo...`);
+    }
+    const isGood = difference > 0 && difference <= 90;
+    const friendlyDate = format(date, 'EEEE d MMMM yyyy hh:mm', { locale });
+    if (!isGood) {
+      console.log(`${numeroRicetta} il ${friendlyDate} è un po' troppo lontano, vero? sono ben ${difference} giorni`);
+    }
+    result.appuntamenti.push({
+      isGood,
+      date,
+      address,
+    });
+  }
+  const found = result.appuntamenti.find(({ isGood }) => isGood);
+  if (!found) {
+    console.log(`${numeroRicetta} Non ho trovato nulla`);
+    await browser.close();
+    return result;
+  }
+  console.log(`${numeroRicetta} Ho trovato qualcosa...`);
+  await page.click('span[aria-describedby="Avanti"] button');
+  await new Promise((r) => setTimeout(r, 5_000));
+  result.image = await page.screenshot({ fullPage: true });
+  // TODO: fill telefono + mail
+  await page.click('span[aria-describedby="Conferma"] button');
+  console.log(`${numeroRicetta} Preso!`);
+  await new Promise((r) => setTimeout(r, 5_000));
+  result.image = await page.screenshot({ fullPage: true });
+  result.confirmed = found;
+  return result;
+}
+
+export default reserve;
