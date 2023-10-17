d40c6d5a88eb550224895a97f286bacece46d588
Andrea Valla <valla.andrea@gmail.com>
2023-10-17 09:01:45 +0200

    wip

--- services/telegram-bot.js
+++ services/telegram-bot.js
@@ -34,10 +34,10 @@ class TelegramBot {
       { command: 'prenota', description: 'prenota <codice fiscale> <ricetta>' },
       { command: 'help', description: 'Mostra un help' },
     ]);
-    this.bot.onText(/\/help/, this._help);
+    this.bot.onText(/\/help/, this._help.bind(this));
     this.bot.onText(
       /\/prenota ((?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]) (010A2[0-9]+)/i,
-      this._reserve
+      this._reserve.bind(this)
     );
   }
   async _help(msg) {
