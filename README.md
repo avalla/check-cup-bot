3e6cc8502d8120c03b2a993fdee43ae126e321df
Andrea Valla <valla.andrea@gmail.com>
2025-02-08 01:33:40 +0100

    feat: enhancements

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# CHECK CUP
+# Check CUP Bot 
 
-# ASL Italy Appointment Booking Bot README
+## ASL Italy Appointment Booking Bot
 
 Use at your own risk!
+
+##
+```shell
+$ apt install libatk1.0-0 libatk-bridge2.0-0 libcups2 libxcomposite1 libxdamage1 libxfixes3 libxrandr2 libpangocairo-1.0-0 libasound.so.2
+```
--- services/telegram-bot.js
+++ services/telegram-bot.js
@@ -1,23 +1,25 @@
+import fs from 'fs';
+import path from 'path';
 import format from 'date-fns/format/index.js';
 import locale from 'date-fns/locale/it/index.js';
 import dotenv from 'dotenv';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow/index.js';
 import Bot from 'node-telegram-bot-api';
 import reserve from './reserve.js';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow/index.js';
 dotenv.config();
 
 const TOKEN = process.env.TELEGRAM_TOKEN;
 
 function printMsgInfo(msg) {
   const chatId = msg.chat.id;
-
   const { username, first_name, last_name } = msg.from;
   console.log(
-    `${format(msg.date*1000, 'd MMMM yyyy H:mm', { locale })}: "${
+    `${format(msg.date * 1000, 'd MMMM yyyy H:mm', { locale })}: "${
       msg.text
     }" in ${chatId} from @${username} (${first_name} ${last_name})`
   );
 }
+
 function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -28,7 +30,24 @@ class TelegramBot {
   constructor() {
     this.bot = new Bot(TOKEN, { polling: true });
     console.log('Bot started... 🚀');
+    this._loadRicette();
   }
+
+  _loadRicette() {
+    const filePath = path.join(__dirname, 'ricette.json');
+    try {
+      const data = fs.readFileSync(filePath, 'utf8');
+      const ricette = JSON.parse(data);
+      ricette.forEach((item) => {
+        const [ricetta, details] = Object.entries(item)[0];
+        this._ricette.set(ricetta, details);
+      });
+      console.log('Ricette loaded:', this._ricette);
+    } catch (error) {
+      console.error('Error loading ricette:', error);
+    }
+  }
+
   async start() {
     await this.bot.setMyCommands([
       { command: 'prenota', description: 'prenota codice_fiscale ricetta giorni filtrocap filtroindirizzo' },
@@ -41,6 +60,7 @@ class TelegramBot {
       this._reserve.bind(this)
     );
   }
+
   async _help(msg) {
     const chatId = msg.chat.id;
     printMsgInfo(msg);
@@ -52,14 +72,15 @@ class TelegramBot {
 - /help: Questo help`
     );
   }
+
   async _status(msg) {
     const chatId = msg.chat.id;
     const results = Array.from(this._ricette.values());
     const text = results
       .filter(result => result?.chatId === chatId)
       .map(result => `**${result.cf} ${result.ricetta}** :: ${result.info}${result.appuntamenti.map(({ date, address, isGoodDate, isGoodPlace}) =>
-      `- ${format(date, 'EEE dd/MM/yy H:mm', { locale })} ${address} || Posizione: ${isGoodPlace ? '✅': '❌'} Data: ${isGoodDate ? '✅': '❌'}`
-    ).join('\n')}`).join('-------');
+        `- ${format(date, 'EEE dd/MM/yy H:mm', { locale })} ${address} || Posizione: ${isGoodPlace ? '✅': '❌'} Data: ${isGoodDate ? '✅': '❌'}`
+      ).join('\n')}`).join('-------');
     await this.bot.sendMessage(chatId, text || 'Ancora nessuna informazione...',  { parseMode: 'Markdown' });
   }
   async _reserve(msg, match) {
@@ -74,38 +95,54 @@ class TelegramBot {
     let result = {};
     let counter = 1;
     let previousMessage;
+    let previousPhotoMessageId;
     await this.bot.sendMessage(chatId, `Ok proverò a cercare una visita ${ricetta} a ${maxDays} giorni di distanza, filtro cap: ${zipFilter || 'N/A'} e filtro indirizzo:${addressFilter ||  'N/A'}`);
+
     while (true) {
       try {
         result = await reserve({ chatId, cf, ricetta, maxDays, zipFilter, addressFilter });
         this._ricette.set(ricetta, result);
+
         if (result.appuntamenti.length > 0) {
           if (previousMessage) {
             await this.bot.deleteMessage(chatId, previousMessage.message_id);
           }
           previousMessage = await this.bot.sendMessage(chatId, `**${result.cf} ${result.ricetta}** :: ${result.info}${result.appuntamenti.map(({ date, address, isGoodDate, isGoodPlace}) =>
-              `- ${format(date, 'EEE dd/MM/yy H:mm', { locale })} ${address} || Posizione: ${isGoodPlace ? '✅': '❌'} Data: ${isGoodDate ? '✅': '❌'}`
+            `- ${format(date, 'EEE dd/MM/yy H:mm', { locale })} ${address} || Posizione: ${isGoodPlace ? '✅': '❌'} Data: ${isGoodDate ? '✅': '❌'}`
           ).join('\n')}`,  { parseMode: 'Markdown' });
+
+          // Send the latest screenshot and delete the previous one
+          if (result.images && result.images.length > 0) {
+            if (previousPhotoMessageId) {
+              await this.bot.deleteMessage(chatId, previousPhotoMessageId);
+            }
+            const photoMessage = await this.bot.sendPhoto(chatId, result.images[0]);
+            previousPhotoMessageId = photoMessage.message_id;
+          }
         }
       } catch (error) {
-        await this.bot.sendMessage(chatId, `Scusa, c\'è stato un errore :( ${error}`);
+        await this.bot.sendMessage(chatId, `Scusa, c'è stato un errore :( ${error}. Riprova più tardi o contatta il supporto.`);
         console.error(error);
+        break;
       }
+
       if (result.confirmed || result.error) {
         break;
       }
-      const minutes = randomIntFromInterval(2, 5)*60;
+
+      const minutes = randomIntFromInterval(2, 5) * 60;
       const seconds = randomIntFromInterval(0, 60);
       if (counter % 50 === 0) {
         await this.bot.sendMessage(
           chatId,
-          `Ho fatto ${counter} tentativi per prenotare la ${ricetta}`
+          `Ho fatto ${counter} tentativi per prenotare la ${ricetta}. Continuo a cercare...`
         );
       }
       console.log(`${ricetta} aspetto ${minutes + seconds} secondi`)
       await new Promise((r) => setTimeout(r, (minutes + seconds) * 1_000));
       counter++;
     }
+
     switch (true) {
       case !!result.confirmed:
         for (const image of result.images) {
@@ -115,19 +152,20 @@ class TelegramBot {
         const friendlyDate = format(result.confirmed.date, 'EEEE d MMMM yyyy H:mm', { locale });
         await this.bot.sendMessage(
           chatId,
-          `Ho prenotato tra ${daysToNow} 🍾\n${cf} ${ricetta}\n${result.info}\n${result.confirmed.address}\n${friendlyDate}`
+          `Prenotazione confermata tra ${daysToNow} 🍾\n${cf} ${ricetta}\n${result.info}\n${result.confirmed.address}\n${friendlyDate}`
         );
         break;
       case !!result.error:
         for (const image of result.images) {
           await this.bot.sendPhoto(chatId, image);
         }
-
         await this.bot.sendMessage(chatId, `Rimuovo ${cf} ${ricetta}\n${result.error}`);
         break;
     }
     this._ricette.delete(ricetta);
   }
+
+
 }
 
 const singletonInstance = new TelegramBot();
